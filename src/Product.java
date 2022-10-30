af30f238e2176265dc9ff61be2f761d8aa5e0c5f
93294 <9329496@mail.ru>
2022-10-31 00:21:11 +0300

    Доработка домашней работы 1 и домашней работы 2

--- src/Product.java
+++ src/Product.java
@@ -3,20 +3,25 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-public class Product extends HashMap {
-    private String nameProducts;
-    private int price;
-    private int num;
+final class Product {
+    private final String nameProduct;      // Название номенклатуры
+    private int price;                     // Цена
+    private int num;                       // Количество товара
+    private boolean chesk;                 //признак купленного товара
     static public Set<Product> productsSet = new HashSet<>();
 
-    public Product(String nameProducts, int price, int num) {
-        this.nameProducts = nameProducts;
+    public Product(String nameProduct, int price, int num) {
+        if (nameProduct == null || nameProduct.isEmpty() || price <= 0 || num < 0) {
+            throw new IllegalArgumentException("Введите корректные данные");
+        }
+        this.nameProduct = nameProduct;
         this.price = price;
         this.num = num;
+        this.chesk = false;        // товар не куплен
     }
 
     public String getNameProducts() {
-        return nameProducts;
+        return nameProduct;
     }
 
     public int getPrice() {
@@ -35,40 +40,42 @@ public class Product extends HashMap {
         this.num = num;
     }
 
+    public boolean isChesk() {
+        return chesk;
+    }
+
+    public void setChesk() {
+        this.chesk = true;
+    }
+
+    public static Set<Product> getProductsSet() {
+        return productsSet;
+    }
+
+    public static void setProductsSet(Set<Product> productsSet) {
+        Product.productsSet = productsSet;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
-
-        boolean ComparisonOfNames = nameProducts.equals(product.nameProducts);
-        return ComparisonOfNames;
+        return Objects.equals(nameProduct, product.nameProduct);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nameProducts);
+        return Objects.hash(nameProduct);
     }
 
     @Override
     public String toString() {
-        return "Product{" +
-                "nameProducts='" + nameProducts + '\'' +
-                ", price=" + price +
-                ", num=" + num +
-                '}';
+        return "Номенклатура - " +
+                "nameProducts='" + nameProduct +
+                ", цена=" + price +
+                ", количетво=" + num + " кг."+
+                ", товар куплен =" + (chesk ? "куплен" : "не куплен");
     }
 
-    static void addHashSet(Product product) {
-        if (productsSet.size() == 0) {
-            productsSet.add(product);
-        } else {
-            for (Product products : productsSet) {
-                if (products.hashCode() == product.hashCode()) {
-                    throw new IllegalArgumentException("Повторяющий элемент");
-                }
-            }
-        }
-        productsSet.add(product);
-    }
 }
